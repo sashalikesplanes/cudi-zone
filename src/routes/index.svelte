19308da989405887d9827600f1f1e03010f2bc45
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-08-09 03:02:56 +0200

    made torrent server

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,4 +2,4 @@
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<video controls src="/video"></video>
+<video controls src="http://206.189.195.140/video"></video>
