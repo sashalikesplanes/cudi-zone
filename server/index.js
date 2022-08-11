ae60b80b5cc82b7f50b53dca1ef45953e298fc9e
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-08-11 12:40:14 +0200

    completed video sync feature

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { handler } from '../build/handler.js';
+import WebSocket from 'ws';
+import expressWs from 'express-ws';
 import WebTorrent from "webtorrent";
 
 const port = 3001;
 const app = express();
+const expressWss = expressWs(app);
 
 let client = new WebTorrent({ uploadLimit: 100000 }); // Limit to a max of 240 GiB per month
 
@@ -12,10 +15,10 @@ const magnetLink = 'magnet:?xt=urn:btih:FB94D1F636CF921A7CD37BA2FFBB3C7FFD680113
 const magnetLink2 = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent';
 const magnetLink3 = 'magnet:?xt=urn:btih:45B3BCBBC084FDC3DEC8E4EB6DA53DDD414310D5&dn=Run+Hide+Fight+-+Action+Eng+Rus+Multi-Subs+1080p+%5BH264-Mp4%5D&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2770%2Fannounce&tr=https%3A%2F%2Finferno.demonoid.is%2Fannounce&tr=udp%3A%2F%2Ftracker.pomf.se%3A80%2Fannounce&tr=udp%3A%2F%2Fbt1.archive.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.altrosky.nl%3A6969%2Fannounce&tr=udp%3A%2F%2Fmts.tvbit.co%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zerobytes.xyz%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker1.bt.moack.co.kr%3A80%2Fannounce&tr=http%3A%2F%2Fvps02.net.orel.ru%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.files.fm%3A6969%2Fannounce&tr=http%3A%2F%2Fopen.acgnxtracker.com%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce';
 
-await addOrFindTorrentByMagnet(client, magnetLink2);
+/* await addOrFindTorrentByMagnet(client, magnetLink2);
 console.log('loaded vid 2');
 await addOrFindTorrentByMagnet(client, magnetLink3);
-console.log('loaded vid 1');
+console.log('loaded vid 1'); */
 
 /* await Promise.all([
                    // addOrFindTorrentByMagnet(client, magnetLink),
@@ -30,7 +33,6 @@ function getTorrentHashFromMagnetURI(magnetURI) {
 
 function findTorrentByHash(client, infoHash) {
   return client.torrents.find((torrent) => {
-    console.log(torrent.infoHash, infoHash)
     return torrent.infoHash.toLowerCase() === infoHash.toLowerCase();
   })
 }
@@ -58,24 +60,59 @@ async function addOrFindTorrentByMagnet(client, magnetURI) {
 
 function getFileFromTorrent(torrent, listOfExtension) {
   return torrent.files.find((file) => {
-    console.log(file.name)
     return listOfExtension.some((ext) => file.name.endsWith(ext));
   })
 }
 
+let playForbidden = false;
+
+app.ws('/state', (ws) => {
+  function sendMessage(msg, data) {
+    expressWss.getWss().clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify({ msg, data }));
+      }
+    });
+  }
+
+  ws.onopen = () => {
+    // init clients in the room
+    console.log('open on server');
+    ws.send('serverInitialMessage');
+  };
+
+  ws.onmessage = async (event) => {
+    const { msg, data } = JSON.parse(event.data);
+    if (msg === 'pause') {
+      playForbidden = true;
+      sendMessage('pause');
+      setTimeout(() => playForbidden = false, 500);
+    } else if (msg === 'play') {
+      if (!playForbidden) sendMessage('play');
+    } else if (msg === 'clientSubmitTorrent') {
+      sendMessage('serverRecievedTorrent');
+      const torrent = await addOrFindTorrentByMagnet(client, data);
+      if (torrent.ready) sendMessage('serverTorrentReady', torrent.infoHash);
+      else torrent.on('ready', () => {
+        sendMessage('serverTorrentReady', torrent.infoHash);
+      });
+    } else if (msg === 'clientCanPlay') {
+      sendMessage('pause');
+    } else if (msg === 'seekedTo') {
+      sendMessage('seekedTo', data);
+    }
+  }
+});
+
 app.get('/video/:hash', async (req, res) => {
   const torrent = findTorrentByHash(client, req.params.hash);
-  console.log(torrent)
   if (!torrent) {
     res.status(404).end('torrent not found');
     return;
   }
 
   try{
-    console.log(torrent);
     const file = getFileFromTorrent(torrent, ['.mp4', '.mkv', '.mka']);
-    console.log('##FILE##');
-    console.log(file)
 
     const CHUNK_SIZE_BYTES = 10 ** 7;
     const videoSizeBytes = file.length;
---
+++ src/routes/movies.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let player: HTMLVideoElement;
+	let ws: WebSocket;
+
+	let state = 'paused';
+	let magnetURI: string;
+	let currentTime: number;
+
+	function sendMessage(msg: string, data?: string) {
+		ws.send(JSON.stringify({ msg, data }));
+	}
+
+	onMount(() => {
+		ws = new WebSocket('ws://' + location.host + '/state');
+		ws.onerror = (event) => {
+			state = 'error';
+			console.error(event);
+		};
+
+		ws.onopen = () => {
+			state = 'connected';
+		};
+
+		ws.onmessage = (event) => {
+			const { msg, data } = JSON.parse(event.data);
+			if (msg === 'serverRecievedTorrent') state = 'server loading';
+			else if (msg === 'serverTorrentReady') {
+				state = 'client loading';
+				player.src = `/video/${data}`;
+			} else if (msg === 'playbackReady') state = 'paused';
+			else if (msg === 'play') {
+				state = 'playing';
+				player.play();
+			} else if (msg === 'pause') {
+				state = 'paused';
+				player.pause();
+			} else if (msg === 'seekedTo') {
+			  currentTime = Number(data);
+			}
+		};
+	});
+</script>
+
+<main class="flex gap-3 p-3">
+	<video
+		bind:this={player}
+    bind:currentTime
+    controls
+    on:pause|preventDefault={() => sendMessage('pause')}
+    on:play|preventDefault={() => sendMessage('play')}
+    on:seeking|preventDefault={() => sendMessage('pause')}
+    on:seeked|preventDefault={() => sendMessage('seekedTo', String(currentTime))}
+		on:canplay={() => sendMessage('clientCanPlay')}
+		class="flex-grow border-base-300 border"
+	/>
+	<div class="w-fit flex flex-col gap-3">
+		<p class="font-bold text-center text-2xl">{state}</p>
+		<p class="font-bold text-center text-2xl">{currentTime}</p>
+		<input
+			bind:value={magnetURI}
+			type="text"
+			class="input w-full bg-base-300"
+			placeholder="magnet..."
+			name="magnet"
+			id="magnet"
+		/>
+		<button
+			class="btn"
+			on:click={() => sendMessage('clientSubmitTorrent', magnetURI)}
+			disabled={state !== 'connected'}>Submit torrent</button
+		>
+		{#if state === 'playing'}
+			<button class="btn" on:click={() => sendMessage('pause')}>Pause</button>
+		{:else if state === 'paused'}
+			<button class="btn" on:click={() => sendMessage('play')}>Play</button>
+		{/if}
+	</div>
+</main>
