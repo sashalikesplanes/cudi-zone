642b4873f685afd4b1fb9a80478c6ff2f6f237aa
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-08-15 22:48:58 +0200

    changes to make one server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 import express from "express";
-// import { handler } from '../build/handler.js';
+import { handler } from '../build/handler.js';
 import websocket from './sync.js';
 import { streamTorrentHandler, addTorrentHandler } from './torrents.js';
 
-const port = 3001;
+const port = 3000;
 const app = express();
 
 app.use(express.json())
 app.get('/video/:hash', streamTorrentHandler);
 app.post('/video', addTorrentHandler)
-// app.use(handler);
+app.use(handler);
 const server = app.listen(port, () => console.log(`express listening on ${port} , serving sveltekit handler`));
 websocket(server);
